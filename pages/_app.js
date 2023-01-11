ec84f428025cd234060856d702a1c4f4c0d19d31
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-01-11 22:19:42 +0800

    Use next font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Poppins } from "@next/font/google";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+      html {
+        font-family ${Poppins.style.fontFamily};
+      }
+    
+    `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }

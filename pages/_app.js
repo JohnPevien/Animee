67973846f21ef05d06a98f298a9e09401bc69511
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-01-11 22:32:58 +0800

    Import poppins font
    - fix error in previous commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import "../styles/globals.css";
 import { Poppins } from "@next/font/google";
 
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+});
 export default function App({ Component, pageProps }) {
   return (
     <>
       <style jsx global>{`
-      html {
-        font-family ${Poppins.style.fontFamily};
-      }
-    
-    `}</style>
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </>
   );

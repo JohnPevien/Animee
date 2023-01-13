f6bdb933295d43ba535e2ef3ae3034a789943ff5
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-01-13 23:34:18 +0800

    Update nav button

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,23 +3,25 @@ import Link from "next/link";
 type Props = {};
 function navigation({}: Props) {
   return (
-    <header className="fixed top-0 w-full p-5 bg-slate-900  backdrop-blur-xl shadow text-white">
+    <header className="fixed top-0 w-full p-5 backdrop-blur-xl  z-30">
       <nav className="flex justify-between items-center container mx-auto">
         <Link href="/" className=" text-2xl uppercase">
           Animee
         </Link>
         <ul className="flex items-center ml-5">
-          <li className="mr-5">
+          <li className="mr-5 text-lg">
             <Link href="/">Home</Link>
           </li>
-          <li className="mr-5">
+          <li className="mr-5 text-lg">
             <Link href="/anime">Anime</Link>
           </li>
-          <li className="mr-5">
+          <li className="mr-5 text-lg">
             <Link href="/manga">Manga</Link>
           </li>
-          <li className="mr-5 text-slate-900 bg-slate-200 hover:bg-slate-300 rounded-xl px-4 py-1">
-            <Link href="/search">Search</Link>
+          <li className="mr-5 text-lg ">
+            <Link href="/search" className="btn-primary">
+              Search
+            </Link>
           </li>
         </ul>
       </nav>

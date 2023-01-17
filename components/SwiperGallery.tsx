57030f1dee7d0e25c80c5a5ff1d4718b63619806
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-01-17 21:56:44 +0800

    Update swiper gallery

--- components/SwiperGallery.tsx
+++ components/SwiperGallery.tsx
@@ -19,27 +19,27 @@ export default function MarqueeGallery({ data }: Props) {
         centeredSlidesBounds={true}
         centerInsufficientSlides={true}
         loop={true}
-        // autoplay={{
-        //   delay: 2000,
-        //   disableOnInteraction: true,
-        // }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: true,
+        }}
         grabCursor={true}
         className="max-w-screen-xl shadow-2xl bg-transparent lg:max-h-50"
         navigation={{
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         }}
-        // breakpoints={{
-        //   991: {
-        //     spaceBetween: 30,
-        //   },
-        //   768: {
-        //     spaceBetween: 20,
-        //   },
-        //   640: {
-        //     spaceBetween: 10,
-        //   },
-        // }}
+        breakpoints={{
+          991: {
+            spaceBetween: 30,
+          },
+          768: {
+            spaceBetween: 20,
+          },
+          640: {
+            spaceBetween: 10,
+          },
+        }}
       >
         {data.data.map((anime, index) => (
           <SwiperSlide key={anime.mal_id} className="w-fit h-80">

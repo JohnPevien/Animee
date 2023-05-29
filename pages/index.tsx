06ff8a64454df55ddc4db94d965a000b6149b188
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-05-29 23:18:53 +0800

    Move the header to its own component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Header from '@components/Header'
 import TopAnimeGallery from '@components/TopAnimeGallery'
 import RecentAnimeGallery from '@components/RecentAnimeGallery'
 import type { RecentAnimeEpisodes, TopAnime } from 'shared/types'
@@ -23,31 +24,8 @@ export default function Home({ topAnime, recentAnime }: Props) {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <section className="bg-dark p-12 lg:pb-0 flex flex-row w-full ">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5 mx-auto container">
-                    <div className="flex flex-col  items-center justify-center">
-                        <h1 className="text-4xl lg:text-7xl  mb-8 font-bold">
-                            Explore the World of{' '}
-                            <span className="text-primary">Anime</span> -
-                            Discover Your Next{' '}
-                            <span className="text-primary">Favorite </span>
-                            Series
-                        </h1>
-                        <h2 className="text-lg md:text-xl max-w-prose text-gray-600 font-semibold">
-                            Browse extensive collection of anime titles, from
-                            classic favorites to the latest releases, and find
-                            the perfect show to watch next.
-                        </h2>
-                    </div>
-                    <div>
-                        <img
-                            src="https://res.cloudinary.com/dka0kjngw/image/upload/v1673789400/ANIMEE/ANIMEE_-_Hero_swg2p0.png"
-                            alt="anime header"
-                            className="h-auto w-full object-cover drop-shadow-[0_0_15px_rgba(23,105,255,0.8)]"
-                        />
-                    </div>
-                </div>
-            </section>
+            <Header />
+
             <section className="px-5 py-12 ">
                 <div className="flex flex-col justify-center items-center gap-12">
                     <h2 className="block text-4xl font-semibold text-white text-left  w-full">

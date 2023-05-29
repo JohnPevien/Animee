e0f4d424e7aabf0f10f76bb82e0ceb556f6954f0
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-05-29 23:20:22 +0800

    Update screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
 
-  theme: {
-    fontFamily: {
-      sans: ["Poppins", "sans-serif"],
+    theme: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+        screens: {
+            xl: '1440px',
+            lg: '1200px',
+            md: '768px',
+            sm: '640px',
+            xs: '320px',
+        },
+        extend: {
+            colors: {
+                dark: '#000000',
+                dark2: '#17191f',
+                primary: 'rgb(23, 105, 255)',
+                'primary-accent': 'rgb(11, 59, 183)',
+            },
+        },
     },
-    extend: {
-      colors: {
-        dark: "#000000",
-        dark2: "#17191f",
-        primary: "rgb(23, 105, 255)",
-        "primary-accent": "rgb(11, 59, 183)",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
+    plugins: [require('@tailwindcss/typography')],
+}
